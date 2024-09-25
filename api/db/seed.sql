7b2a3af6df0a16d8a63f2749ec865b49025ffb36
Qi He <141190680+Qi2412@users.noreply.github.com>
2024-09-25 16:34:36 +1000

    merge to main

---
+++ api/db/seed.sql
@@ -0,0 +1,9 @@
+-- plants category data
+insert into plant_categories (name, description) values 
+('Spring','This is spring plants'),
+('Summer','This is summer plants'),
+('Autumn','This is autumn plants'),
+('Winter','This is winter plants'),
+('Flowers','This is flower plants'),
+('Vegetables','This is vegetables plants'),
+('Trees','This is trees plants');
