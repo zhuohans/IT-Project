f6a5891cfd2d8574e475d74b50714e257588cd5c
yaoyul <143511413+yaoyul@users.noreply.github.com>
2024-10-04 17:50:10 +1000

    configuration update

---
+++ config/SpringBeanConfig.java
@@ -0,0 +1,23 @@
+package com.haste.yzx.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanConfig {
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//Adding an inner interceptor for pagination support in MyBatis
+        return interceptor;
+    }
+}
